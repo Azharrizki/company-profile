932cf8fa510c3d8f1ced3e3c09adf074ee9c7ef3
zhar <muhammad1800016137@webmail.uad.ac.id>
2021-05-26 06:02:16 +0700

    update profile

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
-
-$(document).ready(function($){
+$(document).ready(function ($) {
     /*NAVBAR*/
-    $(window).on("scroll",function(){
-        if($(this).scrollTop() > 90){
+    $(window).on("scroll", function () {
+        if ($(this).scrollTop() > 90) {
             $(".navbar").addClass("navbar-shrink");
-        }
-        else{
+        } else {
             $(".navbar").removeClass("navbar-shrink");
 
         }
@@ -15,15 +13,14 @@ $(document).ready(function($){
 
     /*----Video Youtube----*/
     const videoSrc = $('#player-1').attr("src");
-    $(".video-play-btn, .video-popup").on("click", function(){
-        if($(".video-popup").hasClass("open")){
+    $(".video-play-btn, .video-popup").on("click", function () {
+        if ($(".video-popup").hasClass("open")) {
             $(".video-popup").removeClass("open");
-            $('#player-1').attr("src","");
-        }
-        else{
+            $('#player-1').attr("src", "");
+        } else {
             $(".video-popup").addClass("open");
-            if($('#player-1').attr("src")==''){
-                $('#player-1').attr("src",videoSrc);
+            if ($('#player-1').attr("src") == '') {
+                $('#player-1').attr("src", videoSrc);
             }
         }
     });
@@ -32,22 +29,22 @@ $(document).ready(function($){
 
     /*-----*/
     $('.features-carousel').owlCarousel({
-        loop:true,
-        margin :0,
-        autoplay:true,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-               
+        loop: true,
+        margin: 0,
+        autoplay: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+
             },
-            600:{
-                items:2,
-               
+            600: {
+                items: 2,
+
             },
-            1000:{
-                items:3,
-               
+            1000: {
+                items: 3,
+
             }
         }
     });
@@ -55,63 +52,63 @@ $(document).ready(function($){
 
     /*--Screenshots carousel--*/
     $('.screenshots-carousel').owlCarousel({
-        loop:true,
-        margin:0,
-        autoplay:true,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-               
+        loop: true,
+        margin: 0,
+        autoplay: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+
             },
-            600:{
-                items:2,
-               
+            600: {
+                items: 2,
+
             },
-            1000:{
-                items:3,
-               
+            1000: {
+                items: 3,
+
             }
         }
     });
-    
 
-      /*-- Testimonial--*/
-      $('.testimonials-carousel').owlCarousel({
-        loop:true,
-        margin:0,
+
+    /*-- Testimonial--*/
+    $('.testimonials-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
         //autoplay:true,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,    
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
             },
-            600:{
-                items:2,       
+            600: {
+                items: 2,
             },
-            1000:{
-                items:1,
-               
+            1000: {
+                items: 1,
+
             }
         }
     });
 
-      /*-- TEAM JAGOAN--*/
-      $('.team-carousel').owlCarousel({
-        loop:true,
-        margin:0,
-        autoplay:true,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,    
+    /*-- TEAM JAGOAN--*/
+    $('.team-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        autoplay: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
             },
-            600:{
-                items:2,       
+            600: {
+                items: 2,
             },
-            1000:{
-                items:3,
-               
+            1000: {
+                items: 3,
+
             }
         }
     });
@@ -122,7 +119,7 @@ $(document).ready(function($){
 
     /*--------Navbar Collapse------*/
 
-    $(function(){ 
+    $(function () {
         var navMain = $(".navbar-collapse"); // avoid dependency on #id
         // "a:not([data-toggle])" - to avoid issues caused
         // when you have dropdown inside navbar
@@ -144,12 +141,12 @@ $(document).ready(function($){
 
     const linkPortofolio = document.querySelectorAll('.portofolio_item')
 
-    function activePortofolio(){
-        if(linkPortofolio){
+    function activePortofolio() {
+        if (linkPortofolio) {
             linkPortofolio.forEach(l => l.classList.remove('active-portofolio'))
             this.classList.add('active-portofolio')
         }
     }
-    linkPortofolio.forEach(l => l.addEventListener ('click', activePortofolio) )
-    
+    linkPortofolio.forEach(l => l.addEventListener('click', activePortofolio))
+
 });
